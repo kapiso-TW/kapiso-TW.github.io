0dcdee61dd8d140ed9fcf21118b2d65152941c56
kapiso-TW <000617jason@gmail.com>
2024-10-26 00:00:38 +0800

    ???

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 静态文件托管
 app.use(express.static(path.join(__dirname, 'render')));
 
-// 路由设置
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'render', 'render.html'));
 });
 
-// 启动服务器
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
