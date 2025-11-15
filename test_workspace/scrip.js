6100833a5328fed420b5d946d9c69f102ded097d
kapiso-TW <000617jason@gmail.com>
2025-11-15 23:41:27 +0800

    Initial commit

--- test_workspace/scrip.js
+++
@@ -1,6 +0,0 @@
-const checkbox = document.getElementById("hamburger-toggle");
-const navMenu = document.getElementById("nav-menu");
-
-checkbox.addEventListener("change", () => {
-  navMenu.classList.toggle("open");
-});
