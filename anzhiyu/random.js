70ede550a3485ad086a98aa3755a7503cf557817
kapiso-TW <000617jason@gmail.com>
2026-02-05 00:09:07 +0800

    Site updated: 2026-02-05 00:09:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/03/hello-world/"];function toRandomPost(){
+var posts=["2024/02/24/japan/","2023/10/22/snail/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
