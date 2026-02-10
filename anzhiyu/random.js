cd9a49eec349cb0c0d9221eafead87bb5e726305
kapiso-TW <160386121+kapiso-TW@users.noreply.github.com>
2026-02-10 15:02:51 +0000

    Deploying to main from @ kapiso-TW/Blog-source@290896415a6c15620ebb376459771934539e655b 🚀

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/24/japan/","2023/10/22/snail/"];function toRandomPost(){
+var posts=["2023/10/22/snail/","2024/02/24/japan/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
